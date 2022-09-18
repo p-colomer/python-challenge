97765f51f36a6297fd500ae537e805e738f8fad5
Patricia Colomer <patricia.colomer@gmail.com>
2022-09-18 19:23:40 +0100

    Python Challenge

---
+++ PyBank/main.py
@@ -0,0 +1,58 @@
+import os
+import csv
+
+budget_csv = os.path.join("Resources", "budget_data.csv")
+file_txt  = os.path.join("Analysis", "output.txt")
+
+
+
+with open(budget_csv) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    header = next(csvreader)
+    monthcount = 0
+    net_total = 0
+    changes = 0
+    difference = 0
+    current = 0
+    previous = 0
+
+    line_count = 0
+    greatest_increase = 0
+    greatest_decrease = 0
+    for row in csvreader:
+        
+        monthcount += 1
+        net_total = net_total + int(row[1])
+
+        if line_count == 0:
+
+            previous = float(row[1])
+            line_count += 1
+        else:
+        
+            current = float(row[1])
+            difference = current - previous
+            if difference > greatest_increase:
+                greatest_increase = difference
+                greatest_increase_date = row[0]
+            
+            if difference < greatest_decrease:
+                greatest_decrease = difference
+                greatest_decrease_date = row[0]
+            changes = changes + difference
+            difference = 0
+            previous = current
+
+
+total_changes = changes / (monthcount - 1)
+
+with open(file_txt,"w") as f:
+    print("\nFinancial Analysis \n----------------------------------------", file = f)
+    print("\nTotal months: " , monthcount, file = f)
+    print("\nTotal: $" +  str(round(net_total)), file = f)
+    print("\nAverage change: $" + str(round(total_changes,2)), file = f)
+    print("\nGreatest Increase in Profits: " + greatest_increase_date + " ($"+ str(round(greatest_increase))+")", file = f)
+    print("\nGreatest Decrease in Profits: " + greatest_decrease_date + " ($" + str(round(greatest_decrease))+")", file = f)
+
+
+
---
+++ PyPoll/main.py
@@ -0,0 +1,53 @@
+import os
+import csv
+
+election_csv = os.path.join("Resources", "election_data.csv")
+file_txt  = os.path.join("Analysis", "output.txt")
+file_txt  = os.path.join("Analysis", "output.txt")
+
+with open(election_csv) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    header = next(csvreader)
+
+    votes_count = 0
+    candidates = []
+    list_of_votes = []
+
+    for row in csvreader:
+        votes_count += 1
+        list_of_votes.append(row[2])
+        
+        if row[2] not in candidates:
+            candidates.append(row[2])
+       
+    
+    votes={}
+    most_votes = 0
+    
+    for person in candidates:
+        counter = 0
+
+        for i in list_of_votes:
+            if i == person:
+            
+                counter += 1
+        percentage = counter/votes_count
+        votes[person] = [percentage, counter]
+        
+        if counter >= most_votes:
+            most_votes = counter
+            winner = person
+        
+       
+   
+with open(file_txt,"w") as f:
+    print("\nElection Results \n----------------------------------------", file = f)
+    print("\nTotal Votes: " , votes_count, file = f)
+    print("\n----------------------------------------", file = f)
+    #print(votes, file = f)
+    for k, v in votes.items():
+        print(f"{k:<10} {v[0]: .3%} ({v[1]})", file = f)
+    print("\n----------------------------------------", file = f)
+    print("\nWinner: " +  winner, file = f)
+    print("\n----------------------------------------", file = f)
+    
